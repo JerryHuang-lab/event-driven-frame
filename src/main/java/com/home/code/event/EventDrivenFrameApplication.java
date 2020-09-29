e3e9103eef4e30edb0047d3130e8030e6652c38a
JerryHuang-lab <798457734@qq.com>
2020-09-29 20:05:08 +0800

    「create」创建事件驱动框架

---
+++ src/main/java/com/home/code/event/EventDrivenFrameApplication.java
@@ -0,0 +1,13 @@
+package com.home.code.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenFrameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenFrameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/code/event/EventDrivenFrameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.code.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenFrameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
